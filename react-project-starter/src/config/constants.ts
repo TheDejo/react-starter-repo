cca12cfccc48c1fc4eef168d4a52bd1b6b763625
DEJO <divineogbe@gmail.com>
2023-01-18 12:12:59 +0000

    initial commit

---
+++ react-project-starter/src/config/constants.ts
@@ -0,0 +1,9 @@
+const SCREEN_TEXT = {
+    test: 'Hello from Testing',
+};
+
+const constant = {
+    SCREEN_TEXT,
+};
+
+export default constant;
---
+++ react-project-starter/src/page-components/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import constant from '@/config/constants';
+
+const { SCREEN_TEXT } = constant;
+
+const Test = () => {
+    return <div>{SCREEN_TEXT.test}</div>;
+};
+
+export default Test;
